a6a0849facf55ad53d972aa54f856e34754b6a04
georges <georges.cote@gmail.com>
2017-02-27 17:21:33 -0500

    Added Edge Hex Intersection

---
+++ src/main/java/com/example/models/Edge.java
@@ -0,0 +1,48 @@
+package com.example.models;
+
+
+/**
+ * Created by G on 17/02/27.
+ */
+public class Edge extends Geometry{
+
+    private boolean isOccupied;
+    private EdgeUnit aOccupant;
+
+    public Edge(int x, int y)
+    {
+        super(x,y);
+        isOccupied = false;
+    }
+
+    public boolean getOccupancyFlag()
+    {
+       return isOccupied;
+    }
+
+    public void setOccupant(EdgeUnit pOccupant)
+    {
+        aOccupant = pOccupant;
+        isOccupied = true;
+    }
+    public EdgeUnit removeOccupant()
+    {
+        EdgeUnit tempUnit = aOccupant;
+        aOccupant = null;
+        isOccupied = false;
+        return tempUnit;
+    }
+
+    @Override
+    public Hex getHexNeighbours() {
+        return null; // NOT DONE
+    }
+    @Override
+    public Geometry getIntersectionNeighbours() {
+        return null; // NOT DONE
+    }
+    @Override
+    public Edge getEdgeNeighbours() {
+        return null; // NOT DONE
+    }
+}
