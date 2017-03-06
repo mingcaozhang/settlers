710bfe011b60356685d3ab4ac45b2e4e4a69912b
Dale Mowbray <dale.mowbray@mail.mcgill.ca>
2017-03-06 03:56:58 -0500

    added getByUsername method

--- src/main/java/com/example/repositories/UserRepository.java
+++ src/main/java/com/example/repositories/UserRepository.java
@@ -7,7 +7,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 
 /* Creates a table in the mysql database to hold user information */
-public interface UserRepository extends CrudRepository<User, Long> {
-
+@Transactional
+public interface UserRepository extends CrudRepository<User, Integer> {
+    public User getByUsername( String username);
 }
 
