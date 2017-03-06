578e87f14d41fc859fceb148d5d78c9a3d34751a
Dale Mowbray <dale.mowbray@mail.mcgill.ca>
2017-03-06 03:44:04 -0500

    stuff

--- src/main/java/com/example/forms/Login.java
+++ src/main/java/com/example/forms/Login.java
@@ -1,14 +1,13 @@
 package com.example.forms;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
 public class Login {
-    @NotNull
-    @Size(min=4, max=35)
+
     private String username;
-    @NotNull
-    @Size(min=4, max=35)
+
     private String password;
+
+    private boolean validStatus = true;
 
     public String getUsername()
     {
@@ -29,5 +28,9 @@ public class Login {
     {
         this.password = password;
     }
+
+    public boolean getValidStatus(){ return validStatus;}
+
+    public void setValidStatus(boolean status){ validStatus = status;}
 
 }
