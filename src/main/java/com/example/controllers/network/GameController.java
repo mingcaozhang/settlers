c53d97479da600feec8753549b472bbb6e3714ce
Ming-PC <mingcao.zhang@mail.mcgill.ca>
2017-04-09 21:25:06 -0400

    Added ViewMaritimeTrade and Player's aTradeRates attribute

--- src/main/java/com/example/controllers/network/GameController.java
+++ src/main/java/com/example/controllers/network/GameController.java
@@ -160,6 +160,20 @@ public class GameController {
         pNew.setValid(isValid);
         return pNew;
     }
+
+    @MessageMapping("/activateknight")
+    @SendTo("/topic/knight")
+    public ViewPiece activateKnight(ViewPiece pNew, Principal caller){
+        Player checkee = gameManager.getPlayerFromString(caller.getName());
+        Intersection checker = gameManager.getGame().getBoard().getIntersections().get(pNew.getId());
+        boolean isValid = gameManager.checkActivateEligibility(checker, pNew.getColor());
+        if (isValid){
+            gameManager.payActivation(checkee);
+            gameManager.activateKnight(checker);
+        }
+        pNew.setValid(isValid);
+        return pNew;
+    }
     // SETUP IS FIRST 2 TURNS
 
     @MessageMapping("/setupsettlement")
@@ -295,7 +309,19 @@ public class GameController {
         pTrade.setValid(isValid);
         return pTrade;
     }
-    
+
+    @MessageMapping("/maritimetrade")
+    @SendTo("/topic/maritimetrade")
+    public ViewMaritimeTrade maritimeTrade(ViewMaritimeTrade pTrade, Principal caller){
+        MaritimeTrade aMaritimeTrade = pTrade.toMaritimeTrade();
+        boolean isValid = gameManager.checkMaritimeTradeEligibility(aMaritimeTrade);
+        if (isValid){
+            aMaritimeTrade.execute();
+        }
+        pTrade.setValid(isValid);
+        return pTrade;
+    }
+
     @MessageMapping("/endturn")
     @SendTo("/topic/turninfo")
     public PlayerAndPhase endTurn(Principal user){
